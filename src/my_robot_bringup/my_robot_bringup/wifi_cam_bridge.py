69e5ca23bdb45c84ad41460d29d69d194a2025a3
vinhlephu-lpv <vinhlephu.lpv@gmail.com>
2026-08-26 15:42:35 +0700

    fix: shutdown error trong wifi_cam_bridge

--- src/my_robot_bringup/my_robot_bringup/wifi_cam_bridge.py
+++ src/my_robot_bringup/my_robot_bringup/wifi_cam_bridge.py
@@ -157,7 +157,10 @@ def main(args=None):
         pass
     finally:
         node.destroy_node()
-        rclpy.shutdown()
+        try:
+            rclpy.shutdown()
+        except Exception:
+            pass
 
 
 if __name__ == '__main__':
